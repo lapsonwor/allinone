d8b722773826f86e39510dc6ea501b4bdfa7d1c6
lapsonwor <lapsonwor@gmail.com>
2021-12-13 08:46:08 +0800

    Update safemath.sol

--- solidity/safemath.sol
+++ solidity/safemath.sol
@@ -1,40 +1,47 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./zombieattack.sol";
-import "./erc721.sol";
-import "./safemath.sol";
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
 
-contract ZombieOwnership is ZombieAttack, ERC721 {
-
-  using SafeMath for uint256;
-
-  mapping (uint => address) zombieApprovals;
-
-  function balanceOf(address _owner) external view returns (uint256) {
-    return ownerZombieCount[_owner];
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
   }
 
-  function ownerOf(uint256 _tokenId) external view returns (address) {
-    return zombieToOwner[_tokenId];
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
   }
 
-  function _transfer(address _from, address _to, uint256 _tokenId) private {
-    // 1. Replace with SafeMath's `add`
-    ownerZombieCount[_to]++;
-    // 2. Replace with SafeMath's `sub`
-    ownerZombieCount[_from]--;
-    zombieToOwner[_tokenId] = _to;
-    emit Transfer(_from, _to, _tokenId);
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
   }
 
-  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
-    _transfer(_from, _to, _tokenId);
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
   }
-
-  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
-    zombieApprovals[_tokenId] = _approved;
-    emit Approval(msg.sender, _approved, _tokenId);
-  }
-
 }
